20ad69e19f806e90f13085bf7a260d35e13de7b6
AnicetNgrt <an.nougaret@gmail.com>
2026-04-12 23:03:19 +0000

    Theme: synthwave configuration addition

---
+++ theme-synthwave.js
@@ -0,0 +1,12 @@
+window.THEMES = window.THEMES || {};
+
+window.THEMES.synthwave = {
+  name: 'Synthwave',
+  bodyBackground: 'rgb(18, 8, 40)',
+  stageBackground: '#1a0533',
+  cloudTint: '#ff2d78',
+  cloudOpacityRange: [0.75, 0.92],
+  backgroundTint: '#6a0dad',
+  uiColor: '#00f5ff',
+  description: 'Retro 80s synthwave: neon pinks, deep purples, and electric cyans.'
+};
